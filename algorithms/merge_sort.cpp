cec4a49745365677a622ad824f59b06ca2d8ff05
Taylor Curran <taylorcurran77@gmail.com>
2020-06-23 10:11:14 -0700

    merge_sort.cpp: all tests passing!

--- algorithms/merge_sort.cpp
+++ algorithms/merge_sort.cpp
@@ -15,13 +15,38 @@
 #include <iostream>
 
 /**
- * @brief Sorts the given T array in place using the ordering provided
- * by the comparison object L 
+ * @brief Merges the sublists between left and mid and
+ * mid + 1 and right (inclusive). Helper to merge_sort
+ * 
+ * @note Merges lists in place by accumulating values
+ * at the left of the range
  */
 template<class T>
-void merge_sort(std::vector<T> a)
+void merge(std::vector<T>& a, size_t left, size_t mid, size_t right)
 {
-    merge_sort(a, 0, a.size());  
+    // Proceed through both lists at the same time
+    while(left <= mid && mid < right)
+    {
+        T temp;
+
+        if(a[left] <= a[right])
+            left++;
+
+        else
+        {
+            temp = a[mid + 1];
+            
+            // Shift left list over
+            for(size_t i = mid + 1; i > left; --i)
+                a[i] = a[i - 1];
+            
+            a[left] = temp;
+            ++left;
+            ++mid;
+        }
+    }
+
+    // Remainder is automatically in order
 }
 
 /**
@@ -29,29 +54,34 @@ void merge_sort(std::vector<T> a)
  * indices. Helper function to merge_sort
  */
 template<class T>
-void merge_sort(std::vector<T> a, size_t left, size_t right)
+void merge_sort(std::vector<T>& a, size_t left, size_t right)
 {
     // Base Case
-    if(right - left > 1)
+    if(right - left < 1)
         return;
 
     // Split list in half
+    size_t mid = left + (right - left) / 2;
 
     // Sort each half
+    merge_sort(a, left, mid);
+    merge_sort(a, mid + 1, right);
 
     // Merged sorted halves
+    merge(a, left, mid, right);
 }
 
 /**
- * @brief Merges the sublists between left and mid and
- * mid + 1 and right (inclusive). Helper to merge_sort
+ * @brief Sorts the given T array in place using the ordering provided
+ * by the comparison object L 
  */
 template<class T>
-void merge(std::vector<T> a, size_t left, size_t mid, size_t right)
+void merge_sort(std::vector<T>& a)
 {
-    // Proceed through both lists at the same time
-
-    // Finish off any remaining values
+    if(a.empty()) 
+        return;
+    
+    merge_sort(a, 0, a.size() - 1);  
 }
 
 int main()
@@ -78,13 +108,13 @@ int main()
     merge_sort(v);
     assert(v == std::vector<int>({1,2,3}));
 
-     v = {1,2};                            // [,1,2] -> [1,2,3]
+     v = {3,1,2};                            // [3,1,2] -> [1,2,3]
     merge_sort(v);
     assert(v == std::vector<int>({1,2,3}));
 
-     v = {1,5,4,2,3};                      // [1,5,4,2,3] -> [1,5,4,2,3]
+     v = {1,5,4,2,3};                      // [1,5,4,2,3] -> [1,2,3,4,5]
     merge_sort(v);
-    assert(v == std::vector<int>({1,2}));
+    assert(v == std::vector<int>({1,2,3,4,5}));
     
     std::cout << "All tests passed!" << std::endl;
     exit(0); 
