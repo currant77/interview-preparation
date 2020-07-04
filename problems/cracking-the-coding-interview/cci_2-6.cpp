c00ea484582942f39f412d4e6f954849dcf11a06
Taylor Curran <taylorcurran77@gmail.com>
2020-07-04 15:54:35 -0700

    cci_2-6.cpp

--- problems/cracking-the-coding-interview/cci_2-6.cpp
+++ problems/cracking-the-coding-interview/cci_2-6.cpp
@@ -20,67 +20,48 @@ as backwards. Using a recursive approach. Function should return a
 reference to the last remaining node in the list (by reference) in
 addition to a bool */
 
-#include <cassert>      // assert
-#include <iostream>     // std::cout, std::endl;
+#include <cassert>                          // assert
+#include <iostream>                         // std::cout, std::endl;
 #include "../../data-structures/Node.h"     // SinglyLinkedListNode<T>, print_list<T>
+#include <functional>                       // std::function
 
 template<class T>
 using Node = SinglyLinkedListNode<T>;
 
-/**
- * @brief Recursive helper function. Returns true if
- * the list between `head` and the node _before_ `tail`
- * is a palindrome, and return pointer to `tail` by reference
- */
-template<class T> 
-bool is_palindrome(Node<T>* head, const Node<T>*& tail)
-{
-    // TODO
-    return false;
-}
-
 template<class T>
-bool is_palindrome(Node<T>* head)
+void tst(std::function<bool(Node<T>*)> fn)
 {
-    // TODO
-    return false;
-}
-
-int main()
-{
-    std::cout << "Beginning tests..." << std::endl; 
-
     Node<int>* empty = NULL;            
-    assert(is_palindrome(empty));               // [] -> true
+    assert(fn(empty));               // [] -> true
 
     Node<int>* one = new Node<int>(5);          
-    assert(is_palindrome(one));                 // [5] -> true
+    assert(fn(one));                 // [5] -> true
     delete one; 
 
     Node<char>* two_true = 
         new Node<char>('a', 
         new Node<char>('a'));
-    assert(is_palindrome(two_true));            // ['a','a'] -> true
+    assert(fn(two_true));            // ['a','a'] -> true
     delete two_true; 
     
     Node<char>* two_false = 
         new Node<char>('a', 
         new Node<char>('b'));
-    assert(is_palindrome(two_false));           // ['a','b'] -> false
+    assert(fn(two_false));           // ['a','b'] -> false
     delete two_false;
 
      Node<char>* three_true = 
         new Node<char>('a', 
         new Node<char>('b',
         new Node<char>('a')));
-    assert(is_palindrome(three_true));          // ['a','b','a'] -> true
+    assert(fn(three_true));          // ['a','b','a'] -> true
     delete three_true; 
     
     Node<char>* three_false = 
         new Node<char>('a', 
         new Node<char>('b',
         new Node<char>('c')));
-    assert(is_palindrome(three_false));         // ['a','b','c'] -> false
+    assert(fn(three_false));         // ['a','b','c'] -> false
     delete three_false; 
 
     Node<std::string>* long_true =
@@ -91,9 +72,17 @@ int main()
         new Node<std::string>("c",
         new Node<std::string>("a",
         new Node<std::string>("r")))))));
-    assert(is_palindrome(long_true));         // ['r','a','c','e','c','a','r'] -> true
+    assert(fn(long_true));         // ['r','a','c','e','c','a','r'] -> true
     delete long_true;
+}
+
+int main()
+{
+    std::cout << "Beginning tests..." << std::endl; 
+
+    
 
     std::cout << "All tests passed!" << std::endl;
+    
     exit(0); 
 }
